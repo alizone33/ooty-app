b46bbc94767930898ec3f622fab9a1bc0dc9b735
alizone33 <gdkrockers2020@gmail.com>
2026-06-14 21:27:37 +0530

    Create supabase.js

---
+++ src/supabase.js
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https:// sb_publishable_QDOXEtp0Psx53tRPK4zmaQ_GFIrycKo.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjd3RneWZ5ZWFhdm56Y2R0c2ljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE0NDMyNDIsImV4cCI6MjA5NzAxOTI0Mn0.tSSJsC55XFNpZNejajAqNhung0nx2uH3rs0iewDDj_c";
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey
+);
