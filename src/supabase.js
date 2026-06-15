db3d445bb1784877bb8d49303fa62c36dc022e4b
alizone33 <gdkrockers2020@gmail.com>
2026-06-16 00:32:53 +0530

    Update Supabase URL in supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 export const supabase = createClient(
-  "https://sb_publishable_QDOXEtp0Psx53tRPK4zmaQ_GFIrycKo.supabase.co",
+  "https://acwtgyfyeaavnzcdtsic.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjd3RneWZ5ZWFhdm56Y2R0c2ljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE0NDMyNDIsImV4cCI6MjA5NzAxOTI0Mn0.tSSJsC55XFNpZNejajAqNhung0nx2uH3rs0iewDDj_c"
 );
